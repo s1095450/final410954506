fa371c8b960303668564a86da96f736dd6113d9a
stanley <stanleytwn@gmail.com>
2022-06-16 19:56:00 +0800

    0616final

---
+++ final410954506/final410954506/Controllers/DateController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace final410954506.Controllers
+{
+    public class DateController : Controller
+    {
+        // GET: Date
+        public ActionResult Index(string number)
+        {
+            string result = "";
+          
+          
+                result = "星期"+number;
+          
+
+            ViewBag.result = result;
+            return View();
+        }
+    }
+}
---
+++ final410954506/final410954506/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="final410954506.MvcApplication" Language="C#" %>
